74f3d5c0db59eef3699303edc748e0d5497b25ac
vasilopo <spivass81@gmail.com>
2019-10-18 15:53:11 +0200

    Add components, embl footer

--- src/components/vf-componenet-rollup/scripts.js
+++ src/components/vf-componenet-rollup/scripts.js
@@ -8,10 +8,10 @@
 
 // All VF JS
 import { vfBanner } from 'vf-banner/vf-banner';
-// vfBanner();
+vfBanner();
 
 import { vfMastheadSetStyle } from 'vf-masthead/vf-masthead';
-// vfMastheadSetStyle();
+vfMastheadSetStyle();
 
 import { vfTabs } from 'vf-tabs/vf-tabs';
 // vfTabs();
@@ -24,10 +24,10 @@ import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-co
 import { emblContentHubFetch } from 'embl-content-hub-loader/embl-content-hub-loader__fetch';
 import { emblContentHub } from 'embl-content-hub-loader/embl-content-hub-loader';
 import { emblConditionalEdit } from 'embl-conditional-edit/embl-conditional-edit';
-// emblContentHub();
+emblContentHub();
 
 import { emblBreadcrumbs } from 'embl-breadcrumbs-lookup/embl-breadcrumbs-lookup';
-// emblBreadcrumbs();
+emblBreadcrumbs();
 
 import { emblContentMetaProperties_Read } from 'embl-content-meta-properties/embl-content-meta-properties';
 // No default invokation
